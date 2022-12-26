f6d91b1a791b244a2b1ce51f3cf7e4f385d1ec6c
donavand <danielsdk99@hotmail.com>
2022-12-26 18:57:10 -0500

    added link for phone number.

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -4,6 +4,7 @@ export default function Contact(){
     return(
         <div className="textbox2">
          <h1>Contact</h1>
+         <a href="tel:+1-267-978-5155">1-267-978-5155</a>
         </div>
     )
 }
