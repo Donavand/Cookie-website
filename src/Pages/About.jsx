ad96a4eabd8a59d35a76098e47201dfb713baf65
donavand <danielsdk99@hotmail.com>
2022-12-27 21:53:55 -0500

    added the icons

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,9 +4,9 @@ export default function About() {
     return (
         <div className="textbox">
             <h1>About The Company </h1>
-<p> "My Name is Christopher Bacon and i'm the owner of Bac N Cookies. I started out just
+<p> My Name is Christopher Bacon and i'm the owner of Bac N Cookies. I started out just
     making cookies with my daughter's around the holidays.Then my youngest daughter said that they 
-    were really good so that you should spread gifts with others. "</p>
+    were really good so that you should spread gifts with others. </p>
     </div>
     )
 }
