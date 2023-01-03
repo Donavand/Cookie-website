52051db3a293327c4b448a2abe3adcd6cae65eca
donavand <danielsdk99@hotmail.com>
2023-01-02 23:01:47 -0500

    [main] Added images

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,8 +1,9 @@
 import "./Home.css";
 
 import React from "react";
-
+import Carousel from "react-bootstrap/Carousel";
 import { Center, TextConstants } from "../../common";
+import cookies1 from "./cookies1.jpg";
 
 /**
  * The home page, which contains the welcome message and all information about the website we want to be seen first
@@ -13,5 +14,17 @@ export const Home = (): JSX.Element => (
     <Center>
         <h1>{TextConstants.HOME.TITLE}</h1>
         <div>{TextConstants.HOME.CONTENT}</div>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    src={cookies1}
+                    style={{ height: "800px", width: "100%" }}
+                />
+            </Carousel.Item>
+            <Carousel.Caption>
+                <h3>{"p1"}</h3>
+                <p>{"p1"}</p>
+            </Carousel.Caption>
+        </Carousel>
     </Center>
 );
